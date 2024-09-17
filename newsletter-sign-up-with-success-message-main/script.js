96d2a2feb7f026f928851baddceade48a26924a3
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-09-17 14:31:33 +0600

    project finished

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -1,9 +1,39 @@
 const newsletterCta = document.getElementById("newsletter-cta");
 const form = document.getElementById("form");
+const input = document.getElementById("email");
 const successMessage = document.getElementById("newsletter-success");
+const error = document.getElementById("error");
+const successEmail = document.getElementById("success-email")
+
+// Email validation
+const validateEmail = (email) => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+};
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  newsletterCta.classList.add("hide");
-  successMessage.classList.remove("hide");
-})
+
+  // check there is any value or not
+  if (input.value === "") {
+    return alert("Please Enter Your Email Address")
+  }
+
+  // check email is valid or not
+  if (validateEmail(input.value)) {
+    newsletterCta.classList.add("hide");
+    successMessage.classList.remove("hide");
+    successEmail.innerText = input.value
+  } else {
+    error.classList.remove("hide");
+    input.classList.add("invalid");
+  }
+
+  // remove error message
+  input.addEventListener("input", () => {
+    error.classList.add("hide");
+    input.classList.remove("invalid");
+  });
+
+});
